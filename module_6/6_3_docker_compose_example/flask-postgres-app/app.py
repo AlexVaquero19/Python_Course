0038fdcead6cb7dae2f02ef93b62dcc461810f54
Alejandro <vaquero.alejandro19@gmail.com>
2024-11-26 13:01:55 +0100

    Last changes

--- module_6/6_3_docker_compose_example/flask-postgres-app/app.py
+++ module_6/6_3_docker_compose_example/flask-postgres-app/app.py
@@ -9,5 +9,9 @@ app = Flask(__name__)
 def hello():
     return "Hello, World! This is a Flask app with PostgreSQL."
 
+@app.route('/greet')
+def greet():
+    return "Hello, Greet! This is a new Route."
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
---
+++ module_6/activity_docker_file/activity_docker.py
@@ -0,0 +1,6 @@
+celsius = float(input("Enter celsius to Farenheit: "))
+
+fahrenheit = (celsius * 1.8) + 32
+
+print('%.2fº Celsius is equivalent to: %.2fº Fahrenheit'
+    % (celsius, fahrenheit))
